1bd1c03241227b34c9f966d33ed7cfe094ce9d9a
Juliana Baez <juliana.baez@outlook.com>
2021-05-13 21:58:59 -0300

    agregamos url

--- script.js
+++ script.js
@@ -3,17 +3,39 @@ const btnTxtAside = document.getElementById ('btn-txt-aside')
 const btnTheme = document.getElementById ('btn-theme')
 const asideTxt = document.querySelector ('.aside-txt')
 const asideImg = document.querySelector ('.aside-img')
+const textTheme = document.getElementById ('text-theme')
+const url = document.getElementById ('url')
+const image = document.querySelector ('.image')
 const body = document.body
 
+// ------HEADER
+// --Boton imagen
 btnImgAside.addEventListener('click', ()=> {
     asideImg.classList.remove('hidden')
     asideTxt.classList.add('hidden')
 })
+// --Boton texto
 btnTxtAside.addEventListener('click', ()=> {
     asideImg.classList.add('hidden')
     asideTxt.classList.remove('hidden')
 })
+// --Boton tema oscuro/claro
 btnTheme.addEventListener('click', () => {
     body.classList.toggle('dark');
-    body.classList.toggle('light');
+    body.classList.toggle('light');  
+    if(body.className == 'dark'){
+        textTheme.innerText="Modo claro"
+    }else{
+        textTheme.innerText="Modo oscuro"
+    }
 });
+
+// ------ASIDE
+// --Imagen
+url.addEventListener ('change',  () => {
+    image.style.backgroundImage= (`url("${url.value}")`)   
+
+})
+    // <img src="" alt="" id="img">
+
+
