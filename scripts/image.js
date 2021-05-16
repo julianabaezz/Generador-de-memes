3340acb0b4f5d4115f06ad196e32f5f0c5e63867
Lourdes <lourdes27782@gmail.com>
2021-05-16 13:00:51 -0300

    se completaron filtros y boton de reset

--- scripts/image.js
+++ scripts/image.js
@@ -9,6 +9,15 @@ const inputRange= document.getElementsByClassName ('input-range')
 const brightnessFilter= document.getElementById('brightness')
 const opacityFilter = document.getElementById('opacity')
 const contrastFilter =document.getElementById ('contrast')
+const blurFilter = document.getElementById ('blur')
+const grayscaleFilter = document.getElementById('grayscale')
+const sepiaFilter = document.getElementById('sepia')
+const hueFilter = document.getElementById('hue')
+const saturateFilter = document.getElementById('saturate')
+const invertFilter = document.getElementById('invert')
+//const resetBtn = document.getElementById('reset-btn')
+//const imageForm = document.getElementById('imageForm')
+
 url.addEventListener ('change',  () => {
     image.style.backgroundImage= (`url("${url.value}")`);
     image.style.backgroundSize= 'cover'
@@ -25,13 +34,20 @@ optionsFilterBg.addEventListener('change', () => {
 
 })
 const updateFilter = () => {
-    image.style.filter= `brightness(${brightnessFilter.value}) opacity(${opacityFilter.value}) contrast(${contrastFilter.value}%)`
+    image.style.filter= `brightness(${brightnessFilter.value}) opacity(${opacityFilter.value}) contrast(${contrastFilter.value}%) blur(${blurFilter.value}px) grayscale(${grayscaleFilter.value}%) sepia(${sepiaFilter.value}%) hue-rotate(${hueFilter.value}deg) saturate(${saturateFilter.value}%) invert(${invertFilter.value})`
 }
 brightnessFilter.addEventListener('input', updateFilter)
 opacityFilter.addEventListener('input', updateFilter)
-contrastFilter.addEventListener ('input', updateFilter)
+contrastFilter.addEventListener('input', updateFilter)
+blurFilter.addEventListener('input', updateFilter )
+grayscaleFilter.addEventListener('input', updateFilter)
+sepiaFilter.addEventListener('input', updateFilter)
+hueFilter.addEventListener('input', updateFilter)
+saturateFilter.addEventListener('input', updateFilter)
+invertFilter.addEventListener('input', updateFilter)
 
-
-
+// resetBtn.addEventListener('click', () => {
+//     imageForm.reset()
+// } )
 
 
