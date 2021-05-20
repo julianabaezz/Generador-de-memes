894e4349fab508ba41ca4dbf59fadb93ed473861
Lourdes <lourdes27782@gmail.com>
2021-05-20 03:02:31 -0300

    se hizo la función para el tipo y el tamaño de la fuente

--- scripts/text.js
+++ scripts/text.js
@@ -7,6 +7,11 @@ const topImage = document.querySelector(".top-text")
 const bottomImage = document.querySelector(".bottom-text")
 const checkTopText = document.getElementById("check-top-text")
 const checkBottomText = document.getElementById("check-bottom-text")
+const textFontTop = document.getElementById('textFontTop')
+const textFontBottom = document.getElementById('textFontBottom')
+const optionTextFont = document.getElementById('optionTextFont')
+const fontSize = document.getElementById('fontSize')
+
 
 topText.addEventListener("keyup", (e) => {
     topImage.innerText = e.target.value
@@ -31,3 +36,15 @@ checkBottomText.addEventListener("change", (e) => {
         bottomImage.style.display = "flex"
     }
 })
+
+optionTextFont.addEventListener('change', () => {
+    textFontTop.style.fontFamily = optionTextFont.value;
+    textFontBottom.style.fontFamily = optionTextFont.value;    
+})
+
+fontSize.addEventListener('change', () => {
+    console.log(fontSize.value);
+    textFontTop.style.fontSize= fontSize.value+'px';
+    textFontBottom.style.fontSize = fontSize.value+'px';    
+})
+
