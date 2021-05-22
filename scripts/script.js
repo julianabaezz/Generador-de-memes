5d361b2d2dd9f24fa38ca3c3e04f171eb6e787c1
Adrián <jasmdp@hotmail.com>
2021-05-22 14:38:28 -0300

    Layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,35 @@
-const body = document.body
-const btnImgAside = document.getElementById ('btn-img-aside')
-const btnTxtAside = document.getElementById ('btn-txt-aside')
-const btnTheme = document.getElementById ('btn-theme')
-const textTheme = document.getElementById ('text-theme')
-const downloadBtn = document.getElementById ('download-btn')
+const body = document.body;
+const btnImgAside = document.getElementById("btn-img-aside");
+const btnTxtAside = document.getElementById("btn-txt-aside");
+const btnTheme = document.getElementById("btn-theme");
+const textTheme = document.getElementById("text-theme");
+const downloadBtn = document.getElementById("download-btn");
 
 // ------HEADER
 // --Boton imagen
-btnImgAside.addEventListener('click', ()=> {
-    asideImg.classList.remove('hidden')
-    asideTxt.classList.add('hidden')
-})
+btnImgAside.addEventListener("click", () => {
+	asideImg.classList.remove("hidden");
+	asideTxt.classList.add("hidden");
+});
 // --Boton texto
-btnTxtAside.addEventListener('click', ()=> {
-    asideImg.classList.add('hidden')
-    asideTxt.classList.remove('hidden')
-})
+btnTxtAside.addEventListener("click", () => {
+	asideImg.classList.add("hidden");
+	asideTxt.classList.remove("hidden");
+});
 // --Boton tema oscuro/claro
-btnTheme.addEventListener('click', () => {
-    body.classList.toggle('dark');
-    body.classList.toggle('light');
-    if(body.className == 'dark'){
-        textTheme.innerText="Modo claro"
-    }else{
-        textTheme.innerText="Modo oscuro"
-    }
+btnTheme.addEventListener("click", () => {
+	body.classList.toggle("dark");
+	body.classList.toggle("light");
+	if (body.className == "dark") {
+		textTheme.innerText = "Modo claro";
+	} else {
+		textTheme.innerText = "Modo oscuro";
+	}
 });
 
 // --Boton descarga
-downloadBtn.addEventListener ("click", ()=> {
-domtoimage.toBlob(image)
-    .then(function (blob) {
-        window.saveAs(blob, 'meme.png');
-    });
-})
+downloadBtn.addEventListener("click", () => {
+	domtoimage.toBlob(image).then(function (blob) {
+		window.saveAs(blob, "meme.png");
+	});
+});
