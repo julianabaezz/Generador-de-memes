fb639d00451473e3c175f4444be8a03b5cc44931
Alexandra Quijano <alexandra.quijano@sofka.com.co>
2021-05-24 17:54:59 -0500

    responsive

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,21 @@ const btnTxtAside = document.getElementById("btn-txt-aside");
 const btnTheme = document.getElementById("btn-theme");
 const textTheme = document.getElementById("text-theme");
 const downloadBtn = document.getElementById("download-btn");
+const contImage = document.querySelector('.wrapper')
+const closeAside = document.getElementById('close-aside');
+const aside = document.querySelector('.aside');
 
 // ------HEADER
 // --Boton imagen
 btnImgAside.addEventListener("click", () => {
+	aside.classList.remove('hidden');
 	asideImg.classList.remove("hidden");
 	asideTxt.classList.add("hidden");
+
 });
 // --Boton texto
 btnTxtAside.addEventListener("click", () => {
+	aside.classList.remove('hidden');
 	asideImg.classList.add("hidden");
 	asideTxt.classList.remove("hidden");
 });
@@ -27,9 +33,14 @@ btnTheme.addEventListener("click", () => {
 	}
 });
 
+//Cerrar Aside
+closeAside.addEventListener('click', () => {
+	aside.classList.add('hidden');
+})
+
 // --Boton descarga
 downloadBtn.addEventListener("click", () => {
-	domtoimage.toBlob(image).then(function (blob) {
+	domtoimage.toBlob(contImage).then(function (blob) {
 		window.saveAs(blob, "meme.png");
 	});
 });
